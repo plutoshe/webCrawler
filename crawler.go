bdc285f4cf76821e3d37a9c6d533eb673bd77935
plutoshe <plutoshe@gmail.com>
2015-08-26 17:32:18 +0800

    master: complete the basic html storage logic

--- crawler.go
+++ crawler.go
@@ -11,22 +11,37 @@ Pckages may be imported:
 */
 import (
 	"fmt"
+	"log"
 	"net/url"
 	"regexp"
-	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/henrylee2cn/pholcus/common/mlog"
 	"github.com/hu17889/go_spider/core/common/page"
 	"github.com/hu17889/go_spider/core/pipeline"
 	"github.com/hu17889/go_spider/core/spider"
+	"github.com/plutoshe/webCrawler/storage"
+	"gopkg.in/mgo.v2"
 )
+
+var collection *mgo.Collection
+
+var base string = "www.dazhongdainping.com"
 
 type MyPageProcesser struct {
 }
 
 func NewMyPageProcesser() *MyPageProcesser {
 	return &MyPageProcesser{}
+}
+
+func checkMatchPattern(base, href string) bool {
+	inDomain := regexp.MustCompile("dianping\\.com")
+	if inDomain.MatchString(href) {
+		// fmt.Println("in  ", href)
+		return true
+	}
+	return false
 }
 
 // Parse html dom here and record the parse result that we want to Page.
@@ -36,66 +51,54 @@ func (this *MyPageProcesser) Process(p *page.Page) {
 		println(p.Errormsg())
 		return
 	}
-	// fmt.Println("json===\n")
-	// fmt.Println(*p.GetJson())
-	// fmt.Println("Rquest===\n")
-	// res2B, _ := json.Marshal(*p.GetRequest())
-	// fmt.Println(string(res2B))
-	// fmt.Println(*p.GetRequest())
-
-	// fmt.Println("URLTAG====\n", p.GetUrlTag())
 	query := p.GetHtmlParser()
-
+	currentUrl := p.GetRequest().GetUrl()
 	var urls []string
 	query.Find("a").Each(func(i int, s *goquery.Selection) {
 		href, _ := s.Attr("href")
-		var absHref string
 		urlHref, err := url.Parse(href)
 		if err != nil {
 			mlog.LogInst().LogError(err.Error())
 			return
 		}
 		if !urlHref.IsAbs() {
-			urlPrefix := p.GetRequest().GetUrl()
-			absHref = urlPrefix + href
-			absHref = absHref
-		} else {
-			fmt.Println(href)
-			inDomain := regexp.MustCompile("dianping\\.com")
-			if inDomain.MatchString(href) {
-				fmt.Println("in  ", href)
-				urls = append(urls, href)
-			}
+			href = currentUrl + href
 		}
-
+		// Temporarily check in crawler.go, it will be implemented in pattern package.
+		if checkMatchPattern(base, href) {
+			urls = urls
+			// urls = append(urls, href)
+		}
 	})
-	// these urls will be saved and crawed by other coroutines.
-	// p.AddTargetRequests(urls, "html")
-	// content, _ := query.Html()
-	// fmt.Println(content)
 
-	name := query.Find(".entry-title .author").Text()
-	name = strings.Trim(name, " \t\n")
-	repository := query.Find(".entry-title .js-current-repository").Text()
-	repository = strings.Trim(repository, " \t\n")
-	//readme, _ := query.Find("#readme").Html()
-	if name == "" {
-		p.SetSkip(true)
-	}
-	// the entity we want to save by Pipeline
-	p.AddField("author", name)
-	p.AddField("project", repository)
-	//p.AddField("readme", readme)
+	// store content to db
+
+	fmt.Println("==store==", currentUrl)
+	content, _ := query.Html()
+	storage.StoreInsert(collection, storage.StoreFormat{currentUrl, content})
+
 }
 
 func (this *MyPageProcesser) Finish() {
-	fmt.Printf("TODO:before end spider \r\n")
+	// fmt.Printf("TODO:before end spider \r\n")
+
 }
 
 func main() {
+	// db initilization
+	dbSession, err := storage.Link2DbByDefault()
+	defer dbSession.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = storage.Link2CollectionByDefault(dbSession)
+
 	// Spider input:
 	//  PageProcesser ;
 	//  Task name used in Pipeline for record;
+	// TO-DO :
+	// Change to goroutine mechanism, use channel to get url
+	// Able to start serveral goroutine to crawler a same website.
 	spider.NewSpider(NewMyPageProcesser(), "TaskName").
 		AddUrl("http://www.dianping.com", "html").  // Start url, html is the responce type ("html" or "json" or "jsonp" or "text")
 		AddPipeline(pipeline.NewPipelineConsole()). // Print result on screen
