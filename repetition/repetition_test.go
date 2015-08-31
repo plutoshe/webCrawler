4102ffb84f6f2ea3afce170152bb6d473d262998
plutoshe <plutoshe@gmail.com>
2015-08-31 16:37:39 +0800

    repetition: update repetition test module
    As change to redis, the test module update.

--- repetition/repetition_test.go
+++ repetition/repetition_test.go
@@ -2,15 +2,38 @@ package repetition
 
 import (
 	"testing"
+
+	"gopkg.in/redis.v3"
 )
 
 func TestRepetiton(t *testing.T) {
-	InitializeVisited()
-	VisitedNewNode("www.baidu.com")
-	if !checkIfVisited("www.baidu.com") {
-		t.Fatal("Repetition check failed : supposed visited node shows non-visited.")
+
+	c := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	rep := &RepetitionJudgement{}
+	rep.InitializeVisited(c, "tt")
+	rep.VisitedNewNode("www.baidu.com")
+	rep.VisitedNewNode("www.dazhongdainping.com")
+	var testdata = []struct {
+		url     string
+		visited bool
+	}{
+		{"www.baidu.com", true},
+		{"123", false},
+		{"www.dazhongdainping.com", true},
+		{"", false},
+		{"www.google.com", false},
 	}
-	if CheckIfVisited("www.google.com") {
-		t.Fatal("Repetition check failed : supposed non-visited node shows visited.")
+	for _, i := range testdata {
+		ex, err := rep.CheckIfVisited(i.url)
+		if err != nil {
+			t.Fatalf("Repetition test failed, error Msg = %v", err)
+		}
+		if i.visited != ex {
+			t.Fatalf("Repetition check failed, node visited status mismatch. Wanted = %t. Get = %t\n", i.visited, !i.visited)
+		}
 	}
 }
